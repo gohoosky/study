16315f040b221cb30c0b085c0ce054b1efe1d779
x5 <x5@wex5-mac.local>
2016-06-03 01:45:02 +0800

    2

--- study/study/ViewController.swift
+++ study/study/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var baritem0: UITabBarItem!
+    
+    @IBOutlet weak var baritem1: UITabBarItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        baritem0.title = "首页"
+        baritem1.title = "分类"
+        
     }
 
     override func didReceiveMemoryWarning() {
